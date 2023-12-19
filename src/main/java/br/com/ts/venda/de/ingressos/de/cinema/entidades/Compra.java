c7579a126bffdc350876e7c00d596353d9e6e58a
Afonso <09afonso09@gmail.com>
2023-12-19 18:48:28 -0300

    feat/Ajustes de BPP

--- src/main/java/br/com/ts/venda/de/ingressos/de/cinema/entidades/Compra.java
+++ src/main/java/br/com/ts/venda/de/ingressos/de/cinema/entidades/Compra.java
@@ -20,43 +20,20 @@ public class Compra {
         this.codigoAssento = codigoAssento;
     }
 
-    public Ingresso comprarIngresso(String codigoAssento) throws Exception {
+    public Ingresso comprarIngresso(String codigoAssento){
 
         if(!sessao.assentoEstaDisponivel(codigoAssento)){
-            throw new Exception("Assento não está disponível");
+            throw new RuntimeException("Assento não está disponível");
         }
 
         long idade = cliente.calcularIdade();
         long faixaEtaria = sessao.getFilme().getFaixaEtaria();
 
         if(idade < faixaEtaria){
-            throw new Exception("Idade menor que a faixa etária");
+            throw new RuntimeException("Idade menor que o esperado. Verifique a classificação indicativa!");
         }
 
-        TipoDia tipoDia = sessao.getTipoDia();
-        TipoSala tipoSala = sessao.getSala().getTipoSala();
-        boolean assentoEhDBOX = sessao.getSala().assentoEhDBOX(codigoAssento);
-
-        if(tipoDia == TipoDia.SEGUNDA || tipoDia == TipoDia.TERCA || tipoDia == TipoDia.QUARTA){
-            if(tipoSala == TipoSala._2D){
-                preco = assentoEhDBOX? Preco.INICIO_SEMANA_2D_DBOX.getPreco() : Preco.INICIO_SEMANA_2D.getPreco();
-            } else {
-                preco = assentoEhDBOX? Preco.INICIO_SEMANA_3D_DBOX.getPreco() : Preco.INICIO_SEMANA_3D.getPreco();
-            }
-        } else if(tipoDia == TipoDia.QUINTA){
-            if(tipoSala == TipoSala._2D){
-                preco = assentoEhDBOX? Preco.MEIO_SEMANA_2D_DBOX.getPreco() : Preco.MEIO_SEMANA_2D.getPreco();
-            } else {
-                preco = assentoEhDBOX? Preco.MEIO_SEMANA_3D_DBOX.getPreco() : Preco.MEIO_SEMANA_3D.getPreco();
-            }
-        } else if(tipoDia == TipoDia.SEXTA || tipoDia == TipoDia.SABADO || tipoDia == TipoDia.DOMINGO
-                || tipoDia == TipoDia.FERIADO || tipoDia == TipoDia.PRE_ESTREIA){
-            if (tipoSala == TipoSala._2D){
-                preco = assentoEhDBOX? Preco.FIM_SEMANA_2D_DBOX.getPreco() : Preco.FIM_SEMANA_2D.getPreco();
-            } else {
-                preco = assentoEhDBOX? Preco.FIM_SEMANA_3D_DBOX.getPreco() : Preco.FIM_SEMANA_3D.getPreco();
-            }
-        }
+        definirPrecoDoIngresso();
 
         CategoriaCliente categoriaCliente = cliente.getCategoria();
         if (categoriaCliente == CategoriaCliente.ESTUDANTE) {
@@ -64,5 +41,40 @@ public class Compra {
         }
 
         return new Ingresso(sessao.getSala().getNumero(), categoriaCliente, codigoAssento, sessao.getData(), sessao.getHorario());
+    }
+
+    private void definirPrecoDoIngresso() {
+        TipoDia tipoDia = sessao.getTipoDia();
+        TipoSala tipoSala = sessao.getSala().getTipoSala();
+        boolean assentoEhDBOX = sessao.getSala().assentoEhDBOX(codigoAssento);
+
+        if(tipoDia == TipoDia.SEGUNDA || tipoDia == TipoDia.TERCA || tipoDia == TipoDia.QUARTA){
+            if(tipoSala == TipoSala._2D){
+                preco = assentoEhDBOX? Preco.INICIO_SEMANA_2D_DBOX.getPreco() : Preco.INICIO_SEMANA_2D.getPreco();
+
+            } else {
+                preco = assentoEhDBOX? Preco.INICIO_SEMANA_3D_DBOX.getPreco() : Preco.INICIO_SEMANA_3D.getPreco();
+
+            }
+
+        } else if(tipoDia == TipoDia.QUINTA){
+            if(tipoSala == TipoSala._2D){
+                preco = assentoEhDBOX? Preco.MEIO_SEMANA_2D_DBOX.getPreco() : Preco.MEIO_SEMANA_2D.getPreco();
+
+            } else {
+                preco = assentoEhDBOX? Preco.MEIO_SEMANA_3D_DBOX.getPreco() : Preco.MEIO_SEMANA_3D.getPreco();
+
+            }
+
+        } else if(tipoDia == TipoDia.SEXTA || tipoDia == TipoDia.SABADO || tipoDia == TipoDia.DOMINGO
+                || tipoDia == TipoDia.FERIADO || tipoDia == TipoDia.PRE_ESTREIA){
+            if (tipoSala == TipoSala._2D){
+                preco = assentoEhDBOX? Preco.FIM_SEMANA_2D_DBOX.getPreco() : Preco.FIM_SEMANA_2D.getPreco();
+
+            } else {
+                preco = assentoEhDBOX? Preco.FIM_SEMANA_3D_DBOX.getPreco() : Preco.FIM_SEMANA_3D.getPreco();
+
+            }
+        }
     }
 }
